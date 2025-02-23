c247b403c472bbe5c3d40402c74519487d9574b4
Jeff Mitchell <jeffreytm@gmail.com>
2025-02-22 22:01:54 -0500

    Update hello.py
    Added comments to the program.

---
+++ hello.py
@@ -0,0 +1,8 @@
+# Hello program using input() function in Python.
+
+name= input("What's your name? ")
+age = input("How old are you? ")
+
+# Second part of program prints name and age using the data from the input.
+print("Hello, " + name + "!")
+print("You are " + age + " years old.")
